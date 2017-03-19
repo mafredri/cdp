a7faee2540eaedce8d15151cc4c831cf33d2e69f
Mathias Fredriksson <mafredri@gmail.com>
2017-03-19 14:55:18 +0200

    rpcc: Move rpc structs and package description

--- rpcc/call.go
+++ rpcc/call.go
@@ -1,5 +1,3 @@
-// Package rpcc implements the communication layer for communicating
-// with a debugging target over the Chrome Debugging Protocol.
 package rpcc
 
 import "context"
--- rpcc/conn.go
+++ rpcc/conn.go
@@ -152,6 +152,53 @@ type Conn struct {
 	streams  map[string]*streamService
 }
 
+type rpcResponse struct {
+	// Response to request.
+	ID     uint64          `json:"id"`     // Echoes that of the rpcRequest.
+	Result json.RawMessage `json:"result"` // Result from invokation, if any.
+	Error  *rpcError       `json:"error"`  // Error, if any.
+
+	// RPC notification from remote.
+	Method string          `json:"method"` // Method invokation requested by remote.
+	Params json.RawMessage `json:"params"` // Method parameters, if any.
+}
+
+func (r *rpcResponse) reset() {
+	r.ID = 0
+	r.Result = nil
+	r.Error = nil
+	r.Method = ""
+	r.Params = nil
+}
+
+func (r *rpcResponse) String() string {
+	if r.Method != "" {
+		return fmt.Sprintf("Method = %s, Params = %s", r.Method, r.Params)
+	}
+	if r.Error != nil {
+		return fmt.Sprintf("ID = %d, Error = %s", r.ID, r.Error.Error())
+	}
+	return fmt.Sprintf("ID = %d, Result = %s", r.ID, r.Result)
+}
+
+type rpcError struct {
+	Code    int64  `json:"code"`
+	Message string `json:"message"`
+	Data    string `json:"data"`
+}
+
+func (e *rpcError) Error() string {
+	var data string
+	if e.Data != "" {
+		data = ", data = " + e.Data
+	}
+	return fmt.Sprintf("rpc error: %s (code = %d%s)", e.Message, e.Code, data)
+}
+
+var (
+	_ error = (*rpcError)(nil)
+)
+
 // recv decodes and handles RPC responses. Respones to RPC requests
 // are forwarded to the pending call, if any. RPC Notifications are
 // forwarded by calling notify, synchronously.
@@ -198,6 +245,12 @@ func (c *Conn) recv(notify func(string, []byte), done chan<- error) {
 			call.done(err)
 		}
 	}
+}
+
+type rpcRequest struct {
+	ID     uint64      `json:"id"`               // ID chosen by client.
+	Method string      `json:"method"`           // Method invoked on remote.
+	Params interface{} `json:"params,omitempty"` // Method parameters, if any.
 }
 
 // send returns after the call has successfully been dispatched over
@@ -293,7 +346,7 @@ func (c *Conn) close(err error) error {
 	}
 	c.mu.Unlock()
 
-	// Conn could be nil if DialContext did not complete.
+	// Conn can be nil if DialContext did not complete.
 	if c.conn != nil {
 		err = c.conn.Close()
 	}
