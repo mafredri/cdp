65c1c63e9baa87f8077645da7763fb98c168db0a
Mathias Fredriksson <mafredri@gmail.com>
2017-03-21 14:01:07 +0200

    cdptype: Cleanup test

--- cdptype/util_test.go
+++ cdptype/util_test.go
@@ -7,7 +7,6 @@ import (
 	"flag"
 	"io"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"testing"
 
@@ -16,65 +15,66 @@ import (
 
 var update = flag.Bool("update", false, "update .golden files")
 
-func open(t testing.TB, name string) (*os.File, func()) {
-	f, err := os.Open(name)
+func TestRuntimeRemoteObject_String(t *testing.T) {
+	in := filepath.Join("testdata", "log.input")
+	input, err := ioutil.ReadFile(in)
 	if err != nil {
 		t.Fatal(err)
 	}
-	return f, func() { f.Close() }
-}
 
-func TestRuntimeRemoteObject_String(t *testing.T) {
-	inputFile, inputClose := open(t, filepath.Join("testdata", "log.input"))
-	defer inputClose()
-
-	input := bufio.NewScanner(inputFile)
+	scanner := bufio.NewScanner(bytes.NewReader(input))
 	var buf bytes.Buffer
 
-	for input.Scan() {
+	for scanner.Scan() {
 		var logEv cdpevent.RuntimeConsoleAPICalledReply
-		err := json.Unmarshal(input.Bytes(), &logEv)
+		err := json.Unmarshal(scanner.Bytes(), &logEv)
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 		for _, arg := range logEv.Args {
 			buf.WriteString(arg.String())
 			buf.WriteByte('\n')
 		}
 	}
-
-	if input.Err() != nil {
-		t.Error(input.Err())
+	if err := scanner.Err(); err != nil {
+		t.Fatal(err)
 	}
 
-	gname := filepath.Join("testdata", "log.golden")
-	if *update {
-		err := ioutil.WriteFile(gname, buf.Bytes(), 0666)
-		if err != nil {
-			t.Fatal(err)
+	out := filepath.Join("testdata", "log.golden")
+	want, err := ioutil.ReadFile(out)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got := buf.Bytes(); !bytes.Equal(got, want) {
+		if *update {
+			err := ioutil.WriteFile(out, got, 0666)
+			if err != nil {
+				t.Error(err)
+			}
+			return
 		}
-		return
+		t.Error("output does not match golden file")
+		showDiff(t, got, want)
 	}
+}
 
-	goldenFile, goldenClose := open(t, gname)
-	defer goldenClose()
-
+func showDiff(t testing.TB, got, want []byte) {
+	gr := bufio.NewReader(bytes.NewReader(got))
+	wr := bufio.NewReader(bytes.NewReader(want))
 	var lineno int
-	br := bufio.NewReader(&buf)
-	gr := bufio.NewReader(goldenFile)
 	for {
 		lineno++
-		b, err1 := br.ReadBytes('\n')
-		g, err2 := gr.ReadBytes('\n')
+		g, err1 := gr.ReadBytes('\n')
+		w, err2 := wr.ReadBytes('\n')
 		if err1 != nil || err2 != nil {
 			if err1 != io.EOF || err2 != io.EOF {
 				t.Error(err1, err2)
 			}
 			break
 		}
-		if !bytes.Equal(b, g) {
-			t.Errorf("line %d does not match golden file", lineno)
-			t.Errorf("got %s; want %s", b[:len(b)-1], g[:len(g)-1])
+		if !bytes.Equal(g, w) {
+			t.Errorf("line %d: got %s; want %s", lineno, g[:len(g)-1], w[:len(w)-1])
 		}
 	}
 }
