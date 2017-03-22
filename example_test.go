0bdb4f78f1dfa9accbf38d40be00e41ae27067e3
Mathias Fredriksson <mafredri@gmail.com>
2017-03-22 18:26:23 +0200

    Improve comments in example

--- example_test.go
+++ example_test.go
@@ -13,55 +13,57 @@ func Example() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	// Connect to WebSocket URL (page) that speaks the Chrome Debugging Protocol.
-	conn, err := rpcc.DialContext(ctx, "ws://localhost:9222/devtools/page/45a887ba-c92a-4cff-9194-d9398cc87e2c")
+	// Connect to Chrome Debugging Protocol target (webSocketDebuggerUrl).
+	debuggerURL := "ws://localhost:9222/devtools/page/45a887ba-c92a-4cff-9194-d9398cc87e2c"
+	conn, err := rpcc.DialContext(ctx, debuggerURL)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer conn.Close() // Close connection when we're done.
+	defer conn.Close() // Must be closed when we are done.
 
 	// Create a new CDP Client that uses conn.
 	c := cdp.NewClient(conn)
 
-	// Enable the Page domain (enables events for the domain).
+	// Enable events on the Page domain.
 	if err = c.Page.Enable(ctx); err != nil {
 		panic(err)
 	}
 
-	// Open client for DOMContentEventFired to block until DOM has fully loaded.
+	// New DOMContentEventFired client will receive and buffer
+	// ContentEventFired events from now on.
 	domContentEventFired, err := c.Page.DOMContentEventFired(ctx)
 	if err != nil {
 		panic(err)
 	}
 	defer domContentEventFired.Close()
 
-	// Initialize the Navigate args with the optional argument Referrer set.
-	navigateArgs := cdp.NewPageNavigateArgs("https://www.google.com").SetReferrer("https://www.google.com")
-	nav, err := c.Page.Navigate(ctx, navigateArgs)
+	// Create the Navigate arguments with the optional Referrer field set.
+	navArgs := cdp.NewPageNavigateArgs("https://www.google.com").SetReferrer("https://duckduckgo.com")
+	nav, err := c.Page.Navigate(ctx, navArgs)
 	if err != nil {
 		panic(err)
 	}
 
-	// Wait for the event.
+	// Block until a DOM ContentEventFired event is triggered.
 	if _, err = domContentEventFired.Recv(); err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("Page loaded with frame ID: %s\n", nav.FrameID)
 
-	// Fetch the document root node. DOM GetDocument only
-	// has optional arguments, we can pass nil here.
+	// Fetch the document root node. We can pass nil here
+	// since this method only takes optional arguments.
 	doc, err := c.DOM.GetDocument(ctx, nil)
 	if err != nil {
 		panic(err)
 	}
 
-	// Get the HTML for the page.
-	outer, err := c.DOM.GetOuterHTML(ctx, cdp.NewDOMGetOuterHTMLArgs(doc.Root.NodeID))
+	// Get the outer HTML for the page.
+	result, err := c.DOM.GetOuterHTML(ctx, cdp.NewDOMGetOuterHTMLArgs(doc.Root.NodeID))
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("HTML: %s\n", outer.OuterHTML)
+	fmt.Printf("HTML: %s\n", result.OuterHTML)
 }
