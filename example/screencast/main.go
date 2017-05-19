80c11aba8ab177a877e3d618384e18635f9e63dc
Mathias Fredriksson <mafredri@gmail.com>
2017-05-19 21:41:08 +0300

    Add basic screencast example in example/screencast

---
+++ example/screencast/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
+
+	"github.com/mafredri/cdp"
+	"github.com/mafredri/cdp/cdpcmd"
+	"github.com/mafredri/cdp/devtool"
+	"github.com/mafredri/cdp/rpcc"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	ctx, cancel := context.WithCancel(context.TODO())
+	defer cancel()
+
+	devt := devtool.New("http://localhost:9222")
+
+	page, err := devt.Get(ctx, devtool.Page)
+	if err != nil {
+		return err
+	}
+
+	conn, err := rpcc.DialContext(ctx, page.WebSocketDebuggerURL)
+	if err != nil {
+		return err
+	}
+
+	c := cdp.NewClient(conn)
+
+	err = c.Page.Enable(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Navigate to GitHub, block until ready.
+	loadEventFired, err := c.Page.LoadEventFired(ctx)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.Page.Navigate(ctx, cdpcmd.NewPageNavigateArgs("https://github.com"))
+	if err != nil {
+		return err
+	}
+
+	_, err = loadEventFired.Recv()
+	if err != nil {
+		return err
+	}
+
+	screencastFrame, err := c.Page.ScreencastFrame(ctx)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		defer screencastFrame.Close()
+
+		frameN := 0
+		for {
+			ev, err := screencastFrame.Recv()
+			if err != nil {
+				log.Printf("Failed to receive ScreencastFrame: %v", err)
+				return
+			}
+			log.Printf("Got frame with sessionID: %d: %+v", ev.SessionID, ev.Metadata)
+
+			err = c.Page.ScreencastFrameAck(ctx, cdpcmd.NewPageScreencastFrameAckArgs(ev.SessionID))
+			if err != nil {
+				log.Printf("Failed to ack ScreencastFrame: %v", err)
+				return
+			}
+
+			// Write to screencast_frame-00000N.png.
+			name := fmt.Sprintf("screencast_frame-%06d.png", frameN)
+			frameN++
+
+			// Write the frame to file (without blocking).
+			go func() {
+				err = ioutil.WriteFile(name, ev.Data, 0644)
+				if err != nil {
+					log.Printf("Failed to write ScreencastFrame to %q: %v", name, err)
+				}
+			}()
+		}
+	}()
+
+	screencastArgs := cdpcmd.NewPageStartScreencastArgs().SetEveryNthFrame(1).SetFormat("png")
+	err = c.Page.StartScreencast(ctx, screencastArgs)
+	if err != nil {
+		return err
+	}
+
+	// Random delay for our screencast.
+	time.Sleep(30 * time.Second)
+
+	err = c.Page.StopScreencast(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
