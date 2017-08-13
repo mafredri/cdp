db2f984aa3746e48b70b0800f2ed9c6aa8bc8a53
Mathias Fredriksson <mafredri@gmail.com>
2017-08-13 10:04:14 +0300

    rpcc: Add / update tests for new stream behavior

--- rpcc/stream_test.go
+++ rpcc/stream_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"strconv"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func newTestStreamConn() (*Conn, context.CancelFunc) {
@@ -78,22 +80,76 @@ func TestStream_RecvAfterConnClose(t *testing.T) {
 	}
 }
 
+func TestStream_RecvMsg(t *testing.T) {
+	params := struct {
+		arg1  string
+		want1 string
+		arg2  []byte
+		want2 []byte
+		arg3  string
+		want3 string
+	}{
+		want1: "hello",
+		want2: []byte(`"raw"`),
+		want3: "",
+	}
+	type fields struct {
+		payload string
+	}
+	type args struct {
+		m interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    interface{}
+		wantErr bool
+	}{
+		{"Receive string as string", fields{`"hello"`}, args{&params.arg1}, &params.want1, false},
+		{"Receive []byte as raw []byte", fields{`"raw"`}, args{&params.arg2}, &params.want2, false},
+		{"Receive int as string error", fields{`42`}, args{&params.arg3}, &params.want3, true},
+	}
+
+	conn, connCancel := newTestStreamConn()
+	defer connCancel()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	s, err := NewStream(ctx, "test", conn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			conn.notify("test", []byte(tt.fields.payload))
+
+			if err := s.RecvMsg(tt.args.m); (err != nil) != tt.wantErr {
+				t.Errorf("Stream.RecvMsg() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !cmp.Equal(tt.args.m, tt.want) {
+				t.Errorf("Stream.RecvMsg(): got %#v, want %#v", tt.args.m, tt.want)
+			}
+		})
+	}
+}
+
 func TestMessageBuffer(t *testing.T) {
 	n := 1000
-	b := &messageBuffer{
-		ch: make(chan []byte, 1),
-	}
+	b := newMessageBuffer()
 
 	go func() {
 		for i := 0; i < n; i++ {
-			b.store([]byte(strconv.Itoa(i)))
+			b.store(&streamMsg{data: []byte(strconv.Itoa(i))})
 		}
 	}()
 
 	i := 0
 	for bi := range b.get() {
 		b.load()
-		if strconv.Itoa(i) != string(bi) {
+		if strconv.Itoa(i) != string(bi.data) {
 			t.Errorf("Got n = %s, want %d", bi, i)
 		}
 		i++
