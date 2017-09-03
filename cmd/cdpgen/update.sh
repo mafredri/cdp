3d27c650341ffb17b6b65d59b6f4e43ad1b72000
Mathias Fredriksson <mafredri@gmail.com>
2017-09-03 23:04:32 +0300

    cdpgen: Support different flavors of base64 in update.sh

--- cmd/cdpgen/update.sh
+++ cmd/cdpgen/update.sh
@@ -1,7 +1,15 @@
+#!/usr/bin/env bash
+
+base64_opts=(-D)
+# Feature detect if base64 uses a -D or -d flag for decoding, macOS normally uses -D.
+if ! base64 -D <<<"" 1>/dev/null 2>&1; then
+	base64_opts=(-d)
+fi
+
 [[ -d protodef ]] || mkdir protodef
 (cd protodef;
     curl -s "https://chromium.googlesource.com/chromium/src/+/master/third_party/WebKit/Source/core/inspector/browser_protocol.json?format=TEXT" \
-        | base64 -D >js_protocol.json
+        | base64 ${base64_opts[@]} >js_protocol.json
     curl -s "https://chromium.googlesource.com/v8/v8.git/+/master/src/inspector/js_protocol.json?format=TEXT" \
-        | base64 -D >browser_protocol.json
+        | base64 ${base64_opts[@]} >browser_protocol.json
 )
