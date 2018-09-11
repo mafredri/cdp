022cf568af6f220a460ccb4e666c792c7ce5171d
Mathias Fredriksson <mafredri@gmail.com>
2018-09-11 23:58:25 +0300

    cmd/cdpgen/lint: Switch to github.com/mafredri/go-lint

--- cmd/cdpgen/lint/name.go
+++ cmd/cdpgen/lint/name.go
@@ -2,6 +2,8 @@ package lint
 
 import (
 	"regexp"
+
+	lint "github.com/mafredri/go-lint"
 )
 
 var (
@@ -9,9 +11,16 @@ var (
 	reURLs = regexp.MustCompile("^(.*)Urls$")
 )
 
+func init() {
+	lint.SetInitialism("DOM", true)
+	lint.SetInitialism("GPU", true)
+	lint.SetInitialism("SSL", true)
+	lint.SetInitialism("MAC", true)
+}
+
 // Name returns a different name if it should be different.
 func Name(name string) (should string) {
-	should = lintName(name)
+	should = lint.Name(name)
 	// Rename SomethingIds to SomethingIDs.
 	should = reIDs.ReplaceAllString(should, "${1}IDs")
 	should = reURLs.ReplaceAllString(should, "${1}URLs")
